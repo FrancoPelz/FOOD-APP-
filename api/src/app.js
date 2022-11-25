2db0a72a7159963264664685415fa148bf2a1280
FrancoPelz <pelaezmontt.fran@gmail.com>
2022-11-25 20:49:28 -0300

    aaa

--- api/src/app.js
+++ api/src/app.js
@@ -14,7 +14,9 @@ server.use(express.json({ limit: '50mb' }));
 server.use(cookieParser());
 server.use(morgan('dev'));
 server.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'http://localhost:3000'); // update to match the domain you will make the request from
+  //res.header('Access-Control-Allow-Origin', 'http://localhost:3000'); // update to match the domain you will make the request from
+  let getOrigin = req.headers.origin;
+  res.header('Access-Control-Allow-Origin', getOrigin);
   res.header('Access-Control-Allow-Credentials', 'true');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
