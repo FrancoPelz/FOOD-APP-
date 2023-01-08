bc8e05a38b13691067b416eed726c0329e6b360e
FrancoPelz <pelaezmontt.fran@gmail.com>
2023-01-08 15:00:32 -0300

    Responsive home

--- client/src/components/home/home.jsx
+++ client/src/components/home/home.jsx
@@ -11,60 +11,69 @@ import SearchBar from "../searchBar/searchBar";
 import Pagination from "../pagination/pagination";
 import Recipe from "../recipe/recipe";
 import Loading from "../loading/loading";
+import Landing from "../landingPage/landingPage";
 
 
 export default function Home() {
-  const recipes = useSelector((state) => state.filteredRecipes) 
+  const recipes = useSelector((state) => state.filteredRecipes)
   const [page, setPage] = useState(1)
   const [perPage, setPerPage] = useState(9)
   const dispatch = useDispatch()
 
   useEffect(() => {
-      dispatch(fetchRecipes())
-  }, [dispatch]) 
+    dispatch(fetchRecipes())
+  }, [dispatch])
   const max = Math.ceil(recipes.length / perPage)
 
-  function handleOnClick(){
-    dispatch(fetchRecipes())   
-    }
+  function handleOnClick() {
+    dispatch(fetchRecipes())
+  }
 
   return (
-    <div className={styles.bkg}>
-      <ul className={styles.nav}>
-        <li ><Link to={'/'} className={styles.title}>FOOD APP</Link></li> 
-        <li><SearchBar  setPage={setPage}/></li>
-        <li ><Link to={'/create'}><button className={styles.createBTN}>Create Recipe</button></Link></li>           
-      </ul>
-      <div className={styles.filter}>
-        <OrderHs/>
-        <OrderABC/>
-        <FilterDiet setPage={setPage}/>
-        <FilterType setPage={setPage}/>
-        <button onClick = {() => handleOnClick()} className={styles.RefBTN}></button>
-      </div> 
-    <br/> 
-    <div className={styles.recipes}>
-      {
-        recipes.length ?
-         recipes
-        .slice((page-1) * perPage, (page-1) * perPage + perPage )
-        .map((r,i) => 
-        <div key={i}>  
-            <Recipe   
-            key={r.id}                           
-            id={r.id}
-            name={r.name}
-            image={r.image}
-            diets={r.diets}
-            dishTypes={r.dishTypes} 
-            />  
-        <br/>
-        </div>):
-        <Loading/>
-      }    
-    </div>
-    <Pagination page={page} setPage={setPage} max={max}/>
-    <br/>     
-    </div>
+    <React.Fragment>
+      <Landing />
+      <a name="menu" id="menu"></a>
+      <div className={styles.content}>
+        <article class={styles.main}>
+          <div className={styles.nav}>
+            <ul>
+              <li ><Link to={'/'} className={styles.title}>FOOD APP</Link></li>
+              <li><SearchBar setPage={setPage} /></li>
+              <li ><Link to={'/create'}><button className={styles.createBTN}>Create Recipe</button></Link></li>
+            </ul>
+            <div className={styles.navInf}>
+              <OrderHs />
+              <OrderABC />
+              <FilterDiet setPage={setPage} />
+              <FilterType setPage={setPage} />
+              <button onClick={() => handleOnClick()} className={styles.RefBTN}></button>
+            </div>
+          </div>
+          <div className={styles.recipes}>
+            {
+              recipes.length ?
+                recipes
+                  .slice((page - 1) * perPage, (page - 1) * perPage + perPage)
+                  .map((r, i) =>
+                    <div key={i}>
+                      <Recipe
+                        key={r.id}
+                        id={r.id}
+                        name={r.name}
+                        image={r.image}
+                        diets={r.diets}
+                        dishTypes={r.dishTypes}
+                      />
+                      <br />
+                    </div>) :
+                <Loading />
+            }
+          </div>
+        </article>
+        <footer class={styles.footer}>
+          <Pagination page={page} setPage={setPage} max={max} />
+        </footer>
+      </div>
+    </React.Fragment>
   )
 }
--- client/src/components/landingPage/landingPage.jsx
+++ client/src/components/landingPage/landingPage.jsx
@@ -4,20 +4,20 @@ import { Link } from "react-router-dom";
 import { fetchRecipes } from "../../redux/actions";
 import styles from '../landingPage/landingPage.module.css'
 
-export default function Landing(){
+export default function Landing() {
 
     const dispatch = useDispatch()
     useEffect(() => {
         dispatch(fetchRecipes())
-    }, [dispatch]) 
+    }, [dispatch])
 
     return (
         <div className={styles.bkg}>
-           <div className={styles.title}>FOOD APP</div>
-           <div className={styles.subtitle}>Create and search your favorite recipes!</div>
-          <Link to={'/home'}>
-           <button className={styles.btn} type="button">Let`s Cook</button>
-          </Link>
+            <div className={styles.title}>FOOD APP</div>
+            <div className={styles.subtitle}>Create and search your favorite recipes!</div>
+            <a href="#menu" >
+                <button  className={styles.btn} type="button">Let`s Cook</button>
+            </a>
         </div>
     )
 }
