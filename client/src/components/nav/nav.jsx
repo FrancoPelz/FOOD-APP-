f215cbbab5bfd29806bbb6772b412beb7e94eaf5
FrancoPelz <pelaezmontt.fran@gmail.com>
2023-02-15 12:17:08 -0300

    useHistory en Home del nav

--- client/src/components/nav/nav.jsx
+++ client/src/components/nav/nav.jsx
@@ -1,8 +1,17 @@
 import React from "react";
 import { useModal } from "../../hooks/useModal";
 import styles from '../nav/navBar.module.css'
+import { useHistory } from "react-router-dom";
 
 export default function NavBar() {
+
+    function HomeButton() {
+        let history = useHistory();
+
+        function handleClick() {
+            history.push("/");
+        }
+    }
 
     const [isOpenModalSearch, openModalSearch, closeModalSearch] = useModal(false);
     const [isOpenModalFilters, openModalFilters, closeModalfilters] = useModal(false);
@@ -16,11 +25,11 @@ export default function NavBar() {
                         <div className={styles.logo_container}>
                             <h2 className={styles.title}>FOOD</h2>
                             <div className={styles.logo}></div>
-                            <h2 className={styles.title}>APP</h2>  
+                            <h2 className={styles.title}>APP</h2>
                         </div>
                         <p>by Franco Pelaez</p>
                     </a>
-                    <a href="/"><h4>Home</h4></a>
+                    <a onClick={handleClick}><h4>Home</h4></a>
                     <a href="/create"><h4>Create</h4></a>
                 </nav>
             </div >
